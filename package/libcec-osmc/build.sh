072c00bf2a5c3c7826dbf35c96bfd5a91f5355ab
Sam Nazarko <email@samnazarko.co.uk>
2016-10-30 23:48:39 +0000

    [package] [libcec-osmc] Update CEC to version 4.0.0; update Vero 1 and Vero 2 support patches
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/libcec-osmc/build.sh
+++ package/libcec-osmc/build.sh
@@ -5,7 +5,7 @@
 
 . ../common.sh
 
-pull_source "https://github.com/Pulse-Eight/libcec/archive/62b8520cf801343d93e8427393d3d929e59d9c7e.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/Pulse-Eight/libcec/archive/5388d3a18f7fe6b230cf7d6e678906e52d166532.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 # Build in native environment
 build_in_env "${1}" $(pwd) "libcec-osmc"
